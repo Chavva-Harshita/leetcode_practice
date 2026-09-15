2afb667f746e19cd19801caa90f7dc316ac703b3
Ch Harshita <harshitachavva9@gmail.com>
2026-09-15 08:01:31 +0530

    Time: 1 ms (96.34%), Space: 47.6 MB (22.55%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -3,13 +3,13 @@ class Solution {
         int totalsum=0;
         for(int i=0;i<nums.length;i++){
             totalsum+=nums[i];
-        }
+        }   //calcualte totalsum once
         int leftsum=0;
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<nums.length;i++){ //now split the array and check if sum of both parts is same or not
             int rightsum=totalsum-leftsum-nums[i];
             if(leftsum==rightsum) return i;
         
-        leftsum+=nums[i];
+        leftsum+=nums[i];//add elements to leftsum
         }
     return -1;
 }
